1f788127987ca1beda339259c511ff97f5d47693
duynii <duytng@gmail.com>
2013-04-04 14:26:40 +1100

    Added dojo_treesmall.html as an example

--- rfd/PartialConcept.js
+++
@@ -1,9 +0,0 @@
-
-// Example class
-define([
-    "dojo/_base/declare",
-    "rfd/ConceptResource"
-], function(declare, ConceptResource){
-    return declare("rfd/PartialConcept_R", ConceptResource, {
-    });
-});
