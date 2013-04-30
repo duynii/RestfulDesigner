7748c406551ec5ddc8223ff27873a497aae93d36
duynii <duynii@gmail.com>
2013-04-30 16:05:07 +1000

    Added drawArrow function, it is basically a line and triangle  in a group, then group transformed. It works.

--- rfd/controller/app_gfx.js
+++ rfd/controller/app_gfx.js
@@ -1,6 +1,7 @@
 define([
     "dojo/dom",
     "dojox/gfx",
+    "dojox/drawing/annotations/Arrow",
     "dojo/on",
     "dojo/keys",
     "dojo/_base/lang",
@@ -24,7 +25,7 @@ define([
     "rfd/module"
     ],
 function(
-            dom, gfx, on, keys, lang, baseArray, baseEvent, 
+            dom, gfx, Arrow, on, keys, lang, baseArray, baseEvent, 
             parser, Button, registry, query, 
             Resource, StaticResource, TemplatedResource, ConceptResource, Representation,
             Concept_R, Collection_R,
@@ -65,6 +66,66 @@ function(
         console.log("renderItem called");
     };
 
+    //eg. drawArrow({start: {x:200,y:200}, end: {x : 335, y : 335}}, surface);
+    var drawArrowFunc = function drawArrow(p, surface) {
+        /////////////////////////////////////////////////////
+        //Create a group that can be manipulated as a whole
+        /////////////////////////////////////////////////////
+        var group = surface.createGroup();
+
+        var x1 = p.start.x,
+            y1=p.start.y,
+            x2 = p.end.x,
+            y2=p.end.y;
+
+        var len = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+
+        var _defaultStroke = {
+            color : "black",
+            style : "solid",
+            width : 1
+        };
+
+        ///////////////////////////
+        //Add a line to the group
+        ///////////////////////////
+        group.createLine({
+            x1 : 0,
+            y1 : 0,
+            x2 : 0+len,
+            y2 : 0
+        })
+        .setStroke(p.stroke || _defaultStroke)
+        ;
+
+        var _arrowHeight = p.arrowHeight || 9;
+        var _arrowWidth = p.arrowWidth || 6;
+
+        /////////////////////////////////////////////////////
+        //Add a custom path that is a triangle to the group
+        /////////////////////////////////////////////////////
+        group.createPath()
+        .moveTo(len-_arrowHeight,0)
+        .lineTo(len-_arrowHeight,-_arrowWidth)
+        .lineTo(len,0)
+        .lineTo(len-_arrowHeight,_arrowWidth)
+        .lineTo(len-_arrowHeight,0)
+        .setStroke(p.stroke || _defaultStroke)
+        .setFill(p.stroke ? p.stroke.color : "black" )
+        ;
+
+        var _rot = Math.asin((y2-y1)/len)*180/Math.PI;
+        if (x2 <= x1) {_rot = 180-_rot;}
+
+        /////////////////////////////////////////////////////////////
+        //Translate and rotate the entire group as a whole
+        /////////////////////////////////////////////////////////////
+        group.setTransform([
+            dojox.gfx.matrix.translate(x1,y1),
+            dojox.gfx.matrix.rotategAt(_rot,0,0)
+        ]);
+    };
+
     var drawFunc = function(id) 
     {
         gfx.renderer = "canvas";
@@ -75,6 +136,8 @@ function(
 
         // Create a circle with a set "blue" color
         surface.createCircle({ cx: 50, cy: 50, rx: 50, r: 25 }).setFill("blue");
+
+        drawArrowFunc({start: {x: 25, y:25}, end: {x:250, y:250} }, surface);
 
         // Crate a circle with a set hex color
         surface.createCircle({ cx: 300, cy: 300, rx: 50, r: 25 }).setFill("#f00");
@@ -108,6 +171,10 @@ function(
             { offset: 0.5, color: "#0072e5" },
             { offset: 1,   color: "#003b80" }
           ]
+        });
+
+        var arrow = new Arrow({
+          arrowStart: {}, arrowEnd: {}
         });
     };
     return {
