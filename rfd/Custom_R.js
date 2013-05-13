2a68daf32c346787b31cfd03a23fa2c8b7871382
duynii <duynii@gmail.com>
2013-05-13 19:57:59 +1000

    Add Custom_R.js, left out.

---
+++ rfd/Custom_R.js
@@ -0,0 +1,8 @@
+// Example class
+define([
+    "dojo/_base/declare",
+    "rfd/ConceptResource"
+], function(declare, ConceptResource){
+    return declare("rfd/Custom_R", ConceptResource, {
+    });
+});
