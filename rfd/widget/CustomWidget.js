ca7fb6cb653c4c5708f257748b01e1ebc64b59e4
duynii <duynii@gmail.com>
2013-05-29 14:39:26 +1000

    Hooked more event to save data in CustomWidget.js

--- rfd/widget/CustomWidget.js
+++ rfd/widget/CustomWidget.js
@@ -6,7 +6,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase",  "dijit/_TemplatedMixin", "di
         "rfd/model/Branch", "rfd/model/Section", "rfd/module/ClassStyle", 
         "dijit/Menu", "dijit/MenuItem", "dijit/form/RadioButton", "dojox/form/CheckedMultiSelect",
         "dojo/on", "dojo/dom", "dojo/aspect", "dojo/_base/fx", "dojo/_base/array", "dojo/_base/lang",
-        "dijit/popup", "dijit/TooltipDialog", "dijit/focus"
+        "dijit/popup", "dijit/TooltipDialog", "dijit/focus", "dojo/json"
         ],
 
     function(declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template, 
@@ -15,7 +15,7 @@ define(["dojo/_base/declare", "dijit/_WidgetBase",  "dijit/_TemplatedMixin", "di
         TemplatedResource, Branch, Section,
         classStyle,
         Menu, MenuItem, RadioButton, CheckedMultiSelect,
-        on, dom, aspect, baseFx, baseArray, lang, popup, TooltipDialog, focusUtil)
+        on, dom, aspect, baseFx, baseArray, lang, popup, TooltipDialog, focusUtil, JSON)
     {
         return declare("CustomWidget",[_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], 
         {
@@ -103,14 +103,18 @@ define(["dojo/_base/declare", "dijit/_WidgetBase",  "dijit/_TemplatedMixin", "di
                 }));
 
                 var regexNode = this.regex.domNode; //widget
-                this.paramSelect.on("change", function(newValue)
+                this.paramSelect.on("change", lang.hitch(this, function(newValue)
                 {
                     domStyle.set(regexNode, "visibility", newValue == 'string' ? "visible" : "hidden");
                     this._saveForm();
-                });
+                }));
+
+                this.methodSelect.on("change", lang.hitch(this, this._saveForm));
+                this.regex.on("change", lang.hitch(this, this._saveForm));
             },
             _saveForm: function()
             {
+
                 if(!this.form.isValid()) {
                     this.setErrorMsg("Invalid form data");
                     return;
@@ -124,16 +128,19 @@ define(["dojo/_base/declare", "dijit/_WidgetBase",  "dijit/_TemplatedMixin", "di
                 {
                     obj = {};
                     obj.type = data.paramSelect;
-                    if(type == 'string') {
+                    if(obj.type == 'string') {
                         obj.regex = data.regex;
                     }
+                    this.resource.param = obj;
                 }
                 else
                 {
                     this.resource.methods.push(data.method);
+                    this.resource.param = null;
                 }
-                this.resource.param = obj;
 
+                i//var json = JSON.stringify(this.resource);
+                //this.setErrorMsg(json);
                 this.resetErrorMsg();
             },
             _showMutualExUI: function(paramMode) {
