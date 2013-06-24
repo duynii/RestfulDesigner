1f2a4671b700b07f9d749b7493d19af00cf6c6e8
duynii <duynii@gmail.com>
2013-06-24 19:33:01 +1000

    Fleshed out PartialWidget, with representation creation, and disabled MethodWidget.

--- rfd/widget/PartialWidget.js
+++ rfd/widget/PartialWidget.js
@@ -4,7 +4,7 @@ define(["dojo/_base/declare",
         "dojo/text!./templates/PartialWidget.html", 
         "dojox/collections/Dictionary",
         "dojo/dom-style", "dojo/dom-geometry", "dojo/dom-construct", 
-        "rfd/TemplatedResource", 
+        "rfd/TemplatedResource", "rfd/widget/MakeRepDialog", "rfd/Representation",
         "rfd/model/Branch", "rfd/model/Section", "rfd/module/ClassStyle", 
         "dijit/Menu", "dijit/MenuItem", 
         "dojo/on", "dojo/dom", "dojo/aspect", "dojo/_base/fx", "dojo/_base/array", "dojo/_base/lang",
@@ -16,7 +16,8 @@ define(["dojo/_base/declare",
         StaticWidget, template, 
         Dictionary,
         domStyle, domGeometry, domConstruct, 
-        TemplatedResource, Branch, Section,
+        TemplatedResource, MakeRepDialog, Representation,
+        Branch, Section,
         classStyle,
         Menu, MenuItem,
         on, dom, aspect, baseFx, baseArray, lang, popup, TooltipDialog, focusUtil)
@@ -29,12 +30,36 @@ define(["dojo/_base/declare",
             init: function(templateResource) 
             {
                 this.inherited(arguments);
+
+                // Do not allow modification of method, as defaults are expected                
+                this.methodWidget.methods = this.resource.methods;
+                this.methodWidget.allowed = [];
+                this.methodWidget.init();
+                this.methodWidget.hideDetails();
+                this.methodWidget.setDisabled(true);
             },
             postCreate: function()
             {
                 this.inherited(arguments);
 
-
+                this.createButton.on("click", lang.hitch(this, function()
+                {
+                    var d = new MakeRepDialog({
+                        concept: this.resource.concept,
+                        onFinish: lang.hitch(this, function(formData)
+                        {
+                            // Override the previous representation
+                            var rep = new Representation(formData.name, formData.fields);
+                            this.resource.setSelectedRep(rep);
+                            //if the representation is not already saved, add it
+                            d.hide();
+                        }),
+                        onHide: function() {
+                            d.destroyRecursive();
+                        }
+                    });
+                    d.show();
+                }));
             }
         });
     }
